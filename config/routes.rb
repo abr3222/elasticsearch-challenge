dc221e9741565dde6aeeae0a0d6eb3413aabaa44
ABR <abr@gmail.com>
2022-07-03 23:45:40 +0500

    - Handle the Categories and courses as well

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :verticals, except: %i[new edit]
+      resources :categories, except: %i[new edit]
+      resources :courses,    except: %i[new edit]
     end
   end
   root to: "application#welcome"
